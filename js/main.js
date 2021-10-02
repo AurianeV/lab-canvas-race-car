436cd7368e7454e4d56980fbb6389bcc2bf2babf
Auriane Viggiano <auriane.vigg@gmail.com>
2021-10-02 16:41:51 +0200

    itération1

--- js/main.js
+++ js/main.js
@@ -12,9 +12,46 @@ function draw() {
   // Iteration 1: road drawing
   //
 
-  // TODO
+  
+    /*ctx.fillRect(10, 70, 1000, 1600);
 
-  //
+    ctx.clearRect(120, 60, 800, 1600);
+    ctx.strokeRect(100, 70, 50, 1600); */
+    
+  
+  
+
+  ctx.fillStyle = 'green';
+  ctx.fillRect(200, 260, 1000, 1600);
+
+  
+
+  ctx.fillStyle = 'grey';
+  ctx.fillRect(290, 260, 625, 1600); 
+  
+  
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(310, 260, 25, 1600);
+
+  /* Deuxième ligne blanche
+  310 + la largeur du gris (625) - la bordure blanche (25) */
+
+  ctx.fillStyle = 'white';
+  ctx.fillRect(870, 260, 25, 1600);
+
+
+  /* 1000 (largeur du vert) / 2  -  25 (largeur du blanc)/2 */
+  ctx.beginPath();
+  ctx.strokeStyle = 'white';
+  ctx.setLineDash([50, 40]);
+  ctx.moveTo(W/2,0);
+  ctx.lineTo(W/2, 1600);
+  ctx.stroke();
+  // ctx.fillRect(600, 260, 25, 1600);
+
+
+  
   // Iteration 2: car drawing
   //
 
@@ -39,6 +76,8 @@ function draw() {
   // TODO
 
 }
+
+draw()
 
 document.onkeydown = function (e) {
   if (!car) return;
